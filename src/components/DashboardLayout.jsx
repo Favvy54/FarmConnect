5959a47df8ced0c6e3f86ee485955d569d4d88f2
Nnoli Arinze Progress <arinzeprogress01@gmail.com>
2026-07-30 02:01:52 +0100

    Update location display in DashboardLayout

--- src/components/DashboardLayout.jsx
+++ src/components/DashboardLayout.jsx
@@ -42,7 +42,7 @@ export default function DashboardLayout({
               </button>
             </PopoverTrigger>
 
-            <PopoverContent className="w-fit">Ikeja, Lagos</PopoverContent>
+            <PopoverContent className="w-fit">{location}</PopoverContent>
           </Popover>
           <Bell className="w-5 h-5 text-ink" />
           <UserCircle className="w-5 h-5 text-ink" />
@@ -74,6 +74,7 @@ export default function DashboardLayout({
           <VendorTopBar
             title={title}
             subtitle={subtitle}
+            location={location}
             rightSlot={topBarRight}
           />
           {children}
